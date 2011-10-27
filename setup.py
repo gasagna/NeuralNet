e31bd0314d628ddcdfc5aac5f582f1c65dfaee04
Davide Lasagna <lasagnadavide@gmail.com>
2011-10-27 22:36:14 +0200

    Added setup.py filee

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup, Extension
+import numpy as np
+
+                        
+
+setup(  name        = "nn",
+        author      =  "Davide Lasagna",
+        author_email = "davide.lasagna@polito.it",
+        license     = "GPL",
+        url         = 'https://github.com/gasagna/NeuralNet',
+        version     = "0.0.1",
+        description = "A feed-forward multi-layer Neural network in pure python.",
+        )
