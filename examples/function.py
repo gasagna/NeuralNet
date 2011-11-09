c0c45b0721043984584a7da08f0436ea34ff43ac
Davide Lasagna <lasagnadavide@gmail.com>
2011-11-09 21:11:20 +0100

    Fixed example code

--- examples/function.py
+++ examples/function.py
@@ -8,9 +8,9 @@ x = np.linspace(-1,1,1000)
 y = np.exp(x)*np.sin(5*x) + np.random.normal(0,0.1,1000)
 
 # create a neural network object
-net = nn.MultiLayerPerceptron( arch=[1,4,1] )
+net = nn.MultiLayerPerceptron( arch=[1,5,1], beta=1, b=1 )
 # train it
-net.train(x.reshape(-1,1), y.reshape(-1,1), 5000, etol=1e-12 )
+err = net.train_quickprop(x.reshape(-1,1), y.reshape(-1,1), n_iterations=5000, eta=0.8, alpha=0.8, etol=1e-12 )
 
 # plot the result
 plot( x, y, '-')
